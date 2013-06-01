bb036faf942e6b68e948113a8018f8f2857fe40b
Maarten Faddegon <me@maartenfaddegon.nl>
2013-06-01 16:03:57 +0200

    find list of paired bluetooth devices

--- phone/src/main/scala/org/scaloid/hello/HelloScaloidActivity.scala
+++ phone/src/main/scala/org/scaloid/hello/HelloScaloidActivity.scala
@@ -2,15 +2,55 @@ package org.scaloid.hello
 
 import org.scaloid.common._
 import android.graphics.Color
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
+import android.content.Intent
 
-class HelloScaloidActivity extends SActivity {
+class HelloScaloidActivity extends SActivity
+{
+    var mText             : STextView        = null
+    var mBluetoothAdapter : BluetoothAdapter = null
+    var mBluetoothDevice  : BluetoothDevice  = null
+    override implicit val tag = LoggerTag("BELT")
 
-    var mText : STextView = null
+    //def toName (d : Object) = { d.asInstanceOf[BluetoothDevice].getName() }
 
     def findBlueTooth () =
     {
-        toast("op zoek")
-        mText.text = "gevonden"
+        var r = ""
+
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        if (mBluetoothAdapter != null)
+        {
+            info("is bluetooth enabled?")
+
+            if(!mBluetoothAdapter.isEnabled())
+            {
+                info("enabling bluetooth...")
+                val enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBluetooth, 0);
+            }
+            info("bluetooth is enabled, get bondend devices")
+
+            var pairedDevices = mBluetoothAdapter.getBondedDevices();
+            if(pairedDevices.size() > 0)
+            {
+                info("compose list of devices")
+
+                val arr   = pairedDevices.toArray
+                val names = arr.map(d => d.asInstanceOf[BluetoothDevice].getName())
+                r         = names.reduceLeft(_+_)
+            }
+        } else
+        {
+            r = "mislukt"
+        }
+                
+        info("done, return r")
+
+        mText.text = r
     }
 
     def buttonClicked () =
