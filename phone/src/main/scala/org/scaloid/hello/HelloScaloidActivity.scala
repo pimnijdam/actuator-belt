6a36ad388965f07c641029fedba18e9e7d0f6685
Maarten Faddegon <me@maartenfaddegon.nl>
2013-06-01 13:27:12 +0200

    add textview as var and write to it from a function

--- phone/src/main/scala/org/scaloid/hello/HelloScaloidActivity.scala
+++ phone/src/main/scala/org/scaloid/hello/HelloScaloidActivity.scala
@@ -5,17 +5,28 @@ import android.graphics.Color
 
 class HelloScaloidActivity extends SActivity {
 
-    def f () = toast("hoi van f")
+    var mText : STextView = null
+
+    def findBlueTooth () =
+    {
+        toast("op zoek")
+        mText.text = "gevonden"
+    }
+
+    def buttonClicked () =
+    {
+        mText = findBlueTooth()
+    }
 
     onCreate {
         contentView = new SVerticalLayout {
             style {
-                case b: SButton => b.textColor(Color.RED).onClick(toast("actuator-belt!"))
+                case b: SButton => b.textColor(Color.RED).onClick(buttonClicked)
                 case t: STextView => t.textSize(10 dip)
                 case v => v.backgroundColor(Color.YELLOW)
             }
 
-            STextView("Waiting for bluetooth devices...")
+            mText = STextView("Waiting for bluetooth devices...")
             SButton(R.string.red)
         }.padding(20 dip)
     }
