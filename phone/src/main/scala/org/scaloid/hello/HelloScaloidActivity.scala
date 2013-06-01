b8d2c08bf83cfb694b29be0c3dc260137d68c99e
Maarten Faddegon <me@maartenfaddegon.nl>
2013-06-01 13:01:13 +0200

    a seperate function

--- phone/src/main/scala/org/scaloid/hello/HelloScaloidActivity.scala
+++ phone/src/main/scala/org/scaloid/hello/HelloScaloidActivity.scala
@@ -5,20 +5,18 @@ import android.graphics.Color
 
 class HelloScaloidActivity extends SActivity {
 
-  onCreate {
-    contentView = new SVerticalLayout {
-      style {
-        case b: SButton => b.textColor(Color.RED).onClick(toast("actuator-belt!"))
-        case t: STextView => t.textSize(10 dip)
-        case v => v.backgroundColor(Color.YELLOW)
-      }
+    def f () = toast("hoi van f")
 
-      STextView("I am 10 dip tall")
-      STextView("Me too")
-      STextView("I am taller than you").textSize(15 dip) // overriding
-      SEditText("Yellow input field")
-      SButton(R.string.red)
-    }.padding(20 dip)
-  }
+    onCreate {
+        contentView = new SVerticalLayout {
+            style {
+                case b: SButton => b.textColor(Color.RED).onClick(toast("actuator-belt!"))
+                case t: STextView => t.textSize(10 dip)
+                case v => v.backgroundColor(Color.YELLOW)
+            }
 
+            STextView("Waiting for bluetooth devices...")
+            SButton(R.string.red)
+        }.padding(20 dip)
+    }
 }
